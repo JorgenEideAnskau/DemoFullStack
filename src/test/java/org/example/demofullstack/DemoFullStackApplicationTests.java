5627ebea4169c046a0689bcd799e6a1b697f9c33
JorgenEideAnskau <83663255+JorgenEideAnskau@users.noreply.github.com>
2026-04-07 15:24:02 +0200

    Init project

---
+++ src/test/java/org/example/demofullstack/DemoFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
